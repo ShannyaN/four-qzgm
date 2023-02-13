7f7fa9eb58e7b5be611d21f96b0276d5d2e87342
Shannya <niveyro101@gmail.com>
2023-02-12 20:56:48 -0500

    Timer added

---
+++ script.js
@@ -0,0 +1,29 @@
+var timeEl = document.querySelector(".time");
+var mainEl = document.getElementById("main");
+
+var secondsLeft = 75;
+function setTime() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function() {
+        timeEl.textContent = "Time: " + secondsLeft;
+        secondsLeft--;
+     
+  
+      if(secondsLeft === 0) {
+        // Stops execution of action at set interval
+        clearInterval(timerInterval);
+        // Calls function to create and append image
+        sendMessage();
+      }
+  
+    }, 1000);
+  }
+
+  function sendMessage() {
+    timeEl.textContent = " ";
+    var done = document.createElement("h2");
+    timeEl.textContent = "You have run out of time."
+    mainEl.appendChild(done);
+  }
+
+  setTime();
