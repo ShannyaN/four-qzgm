5ab46254dd296994af61649c5cfea01e19adc0da
Shannya <niveyro101@gmail.com>
2023-02-14 01:31:30 -0500

    check1

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 //Making Questions
+console.log(window);
 var questions=[];
 var question1= {
     "qu":"What color is the sky?",
     "a":"red",
     "b":"green",
     "c":"blue",
-    "d":"purple"
+    "d":"purple",
+    "corr":"green"
 }
+console.log(question1);
 var question2= {
     "qu":"what is the best show?",
     "a":"TWD",
@@ -37,30 +40,68 @@ function setTime() {
     }, 1000);
 }
 
-  function sendMessage() {
-    timeEl.textContent = "You have run out of time."
-    quest.appendChild(done);
-  }
+function sendMessage() {
+  timeEl.textContent = "You have run out of time."
+  quest.appendChild(done);
+}
 
-  var optEl = document.querySelector("#ops")
-  var opa = document.createElement("button");
-  var opb = document.createElement("button");
-  var opc = document.createElement("button");
-  var opd = document.createElement("button");
+var optEl = document.querySelector("#ops")
+var opa = document.createElement("button");
+opa.setAttribute("id","optionA");
+opa.setAttribute("onClick","check()");
+var opb = document.createElement("button");
+opb.setAttribute("id","optionB");
+opb.setAttribute("onClick","check()");
+var opc = document.createElement("button");
+opc.setAttribute("id","optionC");
+opc.setAttribute("onClick","check()");
+var opd = document.createElement("button");
+opd.setAttribute("id","optionD");
+opd.setAttribute("onClick","check()");
 
-
-
-  beginButton.addEventListener('click', function (event) {
-    setTime();
+beginButton.addEventListener('click', function () {
+  setTime();
+  firstQuestDisp();
+});
+  
+var arr = [opa,opb,opc,opd];
+function firstQuestDisp () {
     var quest = document.querySelector(".message");
-    quest.textContent=question1.qu; 
-    console.log(question1.a);
-    opa.textContent=question1.a;
+    quest.textContent=question1.qu;
+    opa.textContent= question1.a;
     opb.textContent=question1.b;
-    opc.textContent=question1.c;
+    opc.textContent= question1.c;
     opd.textContent=question1.d;
-    optEl.appendChild(opa);
-    optEl.appendChild(opb);
-    optEl.appendChild(opc);
-    optEl.appendChild(opd);
-  });
+    for (var i=0; i<arr.length;i++){
+      optEl.appendChild(arr[i])}
+    check();
+    //check();
+    }
+    /*function checker(){
+      document.addEventListener("click",function() {
+      console.log(event.target)
+    })};*/
+var wins = 0;
+var losses=0;
+function check() {
+  ans = (event.srcElement.id);
+  answer=document.getElementById(ans);
+  vid=ans.textContent
+  let vids=document.getElementById(ans).textContent
+  //vids=textContent(answer);
+  console.log(vids);
+  //console.log(answer, question1.corr);
+  if (ans.textContent==question1.corr) {
+    wins++;
+  } else {
+    losses++;
+  }
+console.log(wins,losses)
+}
+  /* button.onclick('click', function () {
+    if (button.textContent==question[i].corr){
+      console.log("Correct!")
+    }else {
+     console.log("Wrong!")
+}})
+};*/
