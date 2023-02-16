266fc585ad3715303627eaeaae7dc6b80594237b
Shannya <niveyro101@gmail.com>
2023-02-16 02:50:02 -0500

    concyc

--- script4.js
+++ script4.js
@@ -33,64 +33,44 @@ function setTime() {
 }
 //Questions;
 var question1= {
-    "qu":"If a data type is true or false, what kind of value is it?",
-    "a":"array",
-    "b":"string",
-    "c":"Boolean",
-    "d":"liar",
-    "x":"Boolean"
+    "qu":"What color is the sky?",
+    "a":"red",
+    "b":"green",
+    "c":"blue",
+    "d":"purple",
+    "x":"blue"
 }
 var question2= {
-    "qu":"What is the the value of the first index in an array?",
-    "a":"1",
-    "b":"first",
-    "c":"origin",
-    "d":"0",
-    "x":"0"
+    "qu":"what is the best show?",
+    "a":"TWD",
+    "b":"AHS",
+    "c":"peaky blinders",
+    "d":"The Last of Us",
+    "x":"TWD"
 }
 var question3= {
-  "qu":"What kind of file is primarily used for styling html files?",
-  "a":"CSS",
-  "b":"HTML",
-  "c":"JavaScript",
-  "d":"JQuery",
-  "x":"CSS"
+  "qu":"what is feet?",
+  "a":"four",
+  "b":"seven",
+  "c":"what",
+  "d":"rest",
+  "x":"rest"
 }
 var question4= {
-  "qu":"If a variable value changes, it is considered _________.",
-  "a":"static",
-  "b":"dynamic",
-  "c":"stationary",
-  "d":"moving",
-  "x":"dynamic"
+  "qu":"what is red?",
+  "a":"heart",
+  "b":"blood",
+  "c":"cars",
+  "d":"water",
+  "x":"water"
 }
 var questions = [question1, question2, question3, question4]
 console.log(questions);
-var corr = [];
+corr = [];
 for (var v = 0; v<questions.length;v++){
-    corr.push(questions[v].x)}
+    corr.push(questions[v].x)
+}
 console.log(corr);
-var aS = [];
-for (var v = 0; v<questions.length;v++){
-    aS.push(questions[v].a)}
-console.log(aS);
-var bS = [];
-for (var v = 0; v<questions.length;v++){
-    bS.push(questions[v].b)}
-console.log(bS);
-var cS = [];
-for (var v = 0; v<questions.length;v++){
-    cS.push(questions[v].c)}
-console.log(cS);
-var dS = [];
-for (var v = 0; v<questions.length;v++){
-    dS.push(questions[v].d)}
-console.log(dS);
-var quests = [];
-for (var v = 0; v<questions.length;v++){
-    quests.push(questions[v].qu)}
-console.log(quests);
-
 //Function to display the questions
 var quest = document.querySelector(".message");
 var aButton = document.querySelector('#optionA');
@@ -99,24 +79,19 @@ var cButton = document.querySelector('#optionC');
 var dButton = document.querySelector('#optionD');
 
 //Inputs dynamic content
-var i = -1;
+var i = 0;
 
 function dispQ () {
  //while (i<questions.length) {
-    i++;
-    console.log(questions[i])
-    if (i<questions.length) {
-        quest.textContent=quests[i];
-        aButton.textContent=aS[i];
-        bButton.textContent=bS[i];
-        cButton.textContent= cS[i];
-        dButton.textContent=dS[i];
+        quest.textContent=questions[1].qu;
+        aButton.textContent= questions[1].a;
+        bButton.textContent=questions[1].b;
+        cButton.textContent= questions[1].c;
+        dButton.textContent=questions[1].d;
         //i = i+1;
-        press() }}
-    /*else {
-        endGame();
-    }*/
-function press(){
+        next();
+}
+function next(){
     var allButtons = document.getElementsByClassName("options");
     console.log(allButtons);
     for (var el=0;el<allButtons.length;el++){
@@ -133,15 +108,14 @@ function check() {
    // var loci= i;
     var ansId = event.target.id;
     var ansTxt = document.getElementById(ansId).textContent;
-    console.log(corr[i]);
-    console.log("ansTxt:" + ansTxt);
+ /*   console.log(corr[1]);
+    console.log(ansTxt);*/
     res.textContent = " ";
-    if (ansTxt==corr[i]) {
+    if (ansTxt==corr[1]) {
         res.textContent = "Correct!";
         console.log("right");
     } else {
         res.textContent = "Wrong!";
         console.log("wrong");}
         //timeDown(); 
-        dispQ();
 }
