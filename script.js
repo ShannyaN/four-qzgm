993d39cabd60ea9bce671222fb49e1bda4fbdc32
Shannya <niveyro101@gmail.com>
2023-02-19 14:01:56 -0500

    fix timer

--- script.js
+++ script.js
@@ -1,164 +1,162 @@
-//Making Questions
-console.log(window);
-var questions =[
-question1= {
-    "qu":"What color is the sky?",
-    "a":"red",
-    "b":"green",
-    "c":"blue",
-    "d":"purple",
-    "corr":"blue"
-},
-question2= {
-    "qu":"what is the best show?",
-    "a":"TWD",
-    "b":"AHS",
-    "c":"peaky blinders",
-    "d":"The Last of Us"
-},
-question3= {
-  "qu":"what is feet?",
-  "a":"four",
-  "b":"seven",
-  "c":"what",
-  "d":"rest"
-},
-question4= {
-  "qu":"what is red?",
-  "a":"heart",
-  "b":"blood",
-  "c":"cars",
-  "d":"water"
-}]
+//When Begin is Presses- timer starts & first question appears
+var beginButton = document.querySelector('.begin');
+beginButton.addEventListener('click', function () {
+    show();
+    setTime();
+    dispQ(-1);
+    console.log("BB done")
+  });
 
-console.log(questions);
+//Makes Begin button disappear and option buttons appear
+function show() {
+    beginButton.style.display = "none";
+    aButton.classList.remove("hide");
+    bButton.classList.remove("hide");
+    cButton.classList.remove("hide");
+    dButton.classList.remove("hide");
+    console.log("showed");
+}
 
-//Making Variables
-var beginButton = document.querySelector('#begin');
+//Timer
+var secondsLeft =1000;
 var timeEl = document.querySelector("#time");
-var secondsLeft = 5;
-var done = document.createElement("h2");
-
 function setTime() {
+    var doneEl = document.createElement("h2");
     var timerInterval = setInterval(function() {
         timeEl.textContent = "Time: " + secondsLeft;
         secondsLeft--;
-      if(secondsLeft <= 0) {
+        if(secondsLeft <= 0) {
         clearInterval(timerInterval);
-        timeEl.textContent = "You have run out of time."
-        quest.appendChild(done);
-        //terminateGame();
-      }
+        timeEl.textContent = "You have run out of time.";
+        endGame()}
     }, 1000);
-    return secondsLeft;
 }
 
-/*function terminateGame() {
-  if (secondsLeft==0) {
-    //disp GAMEOVER
-  } else {
-    ///disp You scored " " points
+//Questions;
+var question1= {
+    "qu":"If a data type is true or false, what kind of value is it?",
+    "a":"array",
+    "b":"string",
+    "c":"Boolean",
+    "d":"liar",
+    "x":"Boolean"
+}
+var question2= {
+    "qu":"What is the the value of the first index in an array?",
+    "a":"1",
+    "b":"first",
+    "c":"origin",
+    "d":"0",
+    "x":"0"
+}
+var question3= {
+  "qu":"What kind of file is primarily used for styling html files?",
+  "a":"CSS",
+  "b":"HTML",
+  "c":"JavaScript",
+  "d":"JQuery",
+  "x":"CSS"
+}
+var question4= {
+  "qu":"If a variable value changes, it is considered...",
+  "a":"static",
+  "b":"dynamic",
+  "c":"stationary",
+  "d":"moving",
+  "x":"dynamic"
+}
+var question5= {
+    "qu":"When uploading changes to GitHud, what git command should be done last?",
+    "a":"add changes",
+    "b":"pull",
+    "c":"commit",
+    "d":"push",
+    "x":"push"
   }
-  //enter initials
-  //log scores
-}*/
-var t = setTime();
-function timeDown() {
-    t = t-5;
-    var timerInterval2 = setInterval(function() {
-      timeEl.textContent = "Time: " + t;
-      t--;
-    if(t === 0) {
-      clearInterval(timerInterval);
-      sendMessage();
-  }})}
 
-/*function sendMessage() {
-  timeEl.textContent = "You have run out of time."
-  quest.appendChild(done);
-}*/
+var questions = [question1, question2, question3, question4, question5]
+console.log(questions);
 
-var optEl = document.querySelector("#ops")
-var opa = document.createElement("button");
-opa.setAttribute("id","optionA");
-opa.setAttribute("class","options");
-//opa.setAttribute("onClick","check()");
-var opb = document.createElement("button");
-opb.setAttribute("id","optionB");
-opb.setAttribute("class","options");
-//opb.setAttribute("onClick","check()");
-var opc = document.createElement("button");
-opc.setAttribute("id","optionC");
-opc.setAttribute("class","options");
-//opc.setAttribute("onClick","check()");
-var opd = document.createElement("button");
-opd.setAttribute("id","optionD");
-opd.setAttribute("class","options");
-//opd.setAttribute("onClick","check()");
-//console.log(.options)
-beginButton.addEventListener('click', function () {
-  setTime();
-  firstQuestDisp();
-  console.log("begin done")
-});
-
+//Function to select the elements
 var quest = document.querySelector(".message");
-var arr = [opa,opb,opc,opd];
-function firstQuestDisp () {
-    quest.textContent=questions[0].qu;
-    opa.textContent= questions[0].a;
-    opb.textContent=questions[0].b;
-    opc.textContent= questions[0].c;
-    opd.textContent=questions[0].d;
-    for (var i=0; i<arr.length;i++){
-      optEl.appendChild(arr[i])}
-    check();
+var aButton = document.querySelector('#optionA');
+var bButton = document.querySelector('#optionB');
+var cButton = document.querySelector('#optionC');
+var dButton = document.querySelector('#optionD');
+
+//Inputs dynamic content
+function dispQ (i) {
+    i++;
+    ch=0;
+    console.log(questions[i])
+    if (i<questions.length) {
+        quest.textContent=questions[i]['qu'];
+        aButton.textContent=questions[i]['a'];
+        bButton.textContent=questions[i]['b']
+        cButton.textContent= questions[i]['c']
+        dButton.textContent=questions[i]['d']
+        press(i)}
+    else {
+        endGame();
+    }};
+    var allButtons = document.getElementsByClassName("options");
+
+//Click trigger
+function press(ind){
+    console.log(allButtons);
+    for (var el=0;el<allButtons.length;el++){
+        allButtons[el].addEventListener('click',function(){
+            check(ind);
+        },false);
+    }
+}
+
+
+//Checks if what was clicked is correct
+var con=document.querySelector(".options");
+var res = document.querySelector("#result");
+function check(xx) {
+    var ansId = event.target.id;
+    var ansTxt = document.getElementById(ansId).textContent;
+    console.log(questions[xx]["qu"]);
+    console.log("ansTxt:" + ansTxt);
+    res.textContent = " ";
+    if (ansTxt==questions[xx]["x"]) {
+        res.textContent = "Correct!";
+        console.log("right");
+    } else {
+        res.textContent = "Wrong!";
+        console.log("wrong");
+        timeDown() }
+        dispQ(xx);
     }
 
-/*var con=document.querySelector(".buttons")
-var res = document.createElement("h2");
-con.addEventListener("click", check)
-function check(event) {
-  var an = (event.target.id);
-  var ans=document.getElementById(an).textContent;
-  res.textContent = " ";
-  console.log(ans);
-  if (ans==question1.corr) {
-    res.textContent = "Correct!";
-    con.appendChild(res);
-    console.log("right");
-  } else {
-    res.textContent = "Wrong!";
-    con.appendChild(res);
-    console.log("wrong");}
-    //timeDown(); 
-  }
-  console.log(check());
-  next();
-var els;
-els = [quest, opa, opb, opc, opd];
-var targ = document.querySelector(".options");
-console.log("targ :" + targ);*/
-var q=1;
-function next(){
-  while (q<questions.length-1)
-    targ.addEventListener('click', function () {
-      quest.textContent=questions[q].qu;
-      opa.textContent= questions[q].a;
-      opb.textContent=questions[q].b;
-      opc.textContent= questions[q].c;
-      opd.textContent=questions[q].d; 
-      //check();
-      q++;
-  })};
+//Knocks timer down by 5s when answered wrong
+function timeDown(){
+    timeEl.prepend("-5    ");
+    secondsLeft-=5;}
 
- // beginButton.addEventListener('click', function () {
-/*function changeQ(){
-  res.textContent = "";
-  quest.textContent=question1.qu;
-  opa.textContent= question1.a;
-  opb.textContent=question1.b;
-  opc.textContent= question1.c;
-  opd.textContent=question1.d;
-  for (var i=0; i<arr.length;i++){
-    optEl.appendChild(arr[i])}*/
+//End page
+var congrats ;
+function endGame(){
+    var butts = [aButton, bButton, cButton,dButton]
+    for (y =0; y<butts.length;y++){
+        butts[y].style = "display:none";
+    res.textContent = " ";
+    beginButton.classList.remove("begin");
+    beginButton.classList.add("log");
+    var logButton = document.querySelector('.log');
+    logButton.textContent="Log your score"
+    logButton.style.display ="inline";}
+    logButton.addEventListener('click', function () {
+        window.location.replace("scorepg/scores.html")})
+    if (secondsLeft<=0){
+        quest.textContent="GAME OVER. Enter your initials and log your score."
+        score = 0;
+    } else {
+    score = secondsLeft;
+    timeEl.textContent=" ";
+    quest.textContent = "Congratulations! You completed the quiz! You scored " + score + " points. Enter your initials and log your score!";
+    }}
+    //inp
+    //localStorage.setItem(inp, secondsLeft
